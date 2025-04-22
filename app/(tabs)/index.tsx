dbbdde8ec3e463b7aded70b43054b48e058ea61d
2Kelvin <rocketmancodes02@gmail.com>
2025-04-22 15:38:51 +0300

    add page links and bottom tab nav

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Screen
+      </Link>
     </View>
   );
 }
@@ -17,6 +21,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: { // for Text
-    color: '#fff'
+    color: '#fff',
+    paddingBottom: 10,
+  },
+  button: {
+    fontSize: 20,
+    borderRadius: 10,
+    backgroundColor: 'green',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    color: '#fff',
   }
 });
