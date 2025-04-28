a607d243f329d4494df967a95b74cd2df70f554f
2Kelvin <rocketmancodes02@gmail.com>
2025-04-28 13:28:25 +0300

    build the first screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,37 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
-import { Image } from 'expo-image';
+import { View, StyleSheet } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
-const placeHolderImage = require('@/assets/images/background-image.png');
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 /** Home Screen Component */
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About Screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo"/>
+        <Button label="Use this photo"/>
+      </View>
     </View>
   );
 }
 
-// custom styling object for the View and Text components
 const styles = StyleSheet.create({
-  container: { // for View
+  container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: { // for Text
-    color: '#fff',
-    paddingBottom: 10,
+  imageContainer: {
+    flex: 1,
+    paddingTop: 28,
   },
-  button: {
-    fontSize: 20,
-    borderRadius: 10,
-    backgroundColor: 'green',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    color: '#fff',
+  footerContainer: {
+    flex: 1/3,
+    alignItems: 'center',
   }
 });
